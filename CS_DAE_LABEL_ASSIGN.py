90fe7a69315665383f7826183efcf4304ae81b70
csiffert <93627262+csiffert@users.noreply.github.com>
2023-06-27 18:28:50 -0400

    Add files via upload

--- CS_DAE_LABEL_ASSIGN.py
+++ CS_DAE_LABEL_ASSIGN.py
@@ -12,6 +12,7 @@ import glob
 
 #load in 1 file to get the column names
 df1 = pd.read_csv('D:/CSV/SimData_2023.06.13_09.48.56.csv')
+print(df1['System UTC Time'])
 
 #get column names
 column_names = list(df1.columns)
@@ -60,10 +61,16 @@ print(columns_with_one_unique_value)
 print("Columns with only 1 unique value:")
 for column in columns_with_one_unique_value:
     print(column)
+    
+#DROP INF COLUMN: GPS 1 DME Time
+del df_comb['GPS 1 DME Time']
+
+#drop rows that are mostly null
+df_comb.dropna(subset=['Elapsed Time'], inplace=True)
 
 # plot histograms of numeric columns:
 numeric_cols = df_comb.select_dtypes(include=['int', 'float']).columns
-df_comb[numeric_cols].hist(bins=10, figsize=(30, 35))
+df_comb[numeric_cols].hist(bins=10, figsize=(50,50))
 
 print(numeric_cols)
 
@@ -77,6 +84,25 @@ threshold = 0.01
 near_zero_variance_columns = variance[variance <= threshold].index.tolist()
 print(near_zero_variance_columns)
 
+#calc variances
+variances = variance[variance <= threshold]
+#filter columns with near-zero variance
+near_zero_var_df = variances[variances < threshold]
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Create a bar plot
+plt.bar(near_zero_var_df.index,near_zero_var_df.values)
+plt.xlabel('Attribute')
+plt.ylabel('Variance')
+plt.title('Attributes with nearly Zero Variance')
+plt.xticks(rotation=90)
+plt.xticks(fontsize=7)
+
+#drop near zero variance columns
+
+
 ####################################
 #CS Attempt at "automated" labeling of dataset
 ####################################
@@ -88,6 +114,9 @@ label_table = pd.DataFrame({
     'EndTime': ['13:22:25.0', '13:25:38.0', '13:29:40.0', '11:56:38.0', '11:58:24.0', '12:00:00.0', '16:10:12.0', '16:11:46.0', '16:14:29.0', '13:43:35.0', '13:44:18.0', '13:45:30.0', '12:08:35.0', '12:09:52.0', '12:11:18.0', '16:34:42.0', '16:35:27.0', '16:38:36.0'],  # Replace with actual end time of maneuvers
     'Label': ['Dynamic Rollover', 'Dynamic Rollover', 'Dynamic Rollover', 'Dynamic Rollover', 'Dynamic Rollover', 'Dynamic Rollover', 'Dynamic Rollover', 'Dynamic Rollover', 'Dynamic Rollover', 'LOW-G', 'LOW-G', 'LOW-G', 'LOW-G', 'LOW-G', 'LOW-G', 'LOW-G', 'LOW-G', 'LOW-G']  # Replace with maneuver names
 })
+
+#Add new column
+df_comb['Label'] = 'None'
 
 # Convert date, start time, and end time columns to datetime type
 label_table['Date'] = pd.to_datetime(label_table['Date'])
@@ -119,5 +148,72 @@ print(df_comb[df_comb['Label'].notnull()])
 
 #count of each distinct value in new Label column
 value_counts = df_comb['Label'].value_counts()
+print("Count of number of rows for each label:")
 print(value_counts)
 
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#count of labels
+label_counts = df_comb['Label'].value_counts()
+
+######################################
+# Create a bar plot of maneuver counts
+plt.bar(label_counts.index.astype(str), label_counts.values, color='green')
+
+# Add Values above each bar in the graph
+for i, v in enumerate(label_counts.values):
+    plt.text(i, v, str(v), ha='center', va='bottom')
+
+# Add labels and title to the chart
+#plt.xlabel('Maneuvers')
+plt.ylabel('Count')
+plt.title('Bar Chart of Maneuvers')
+
+# Display the chart
+plt.show()
+
+######################################
+# Create a bar plot of maneuver counts -- excluding None
+
+#filter DF to only include Dynamic Rollovers and Low-G
+filtered_df = df_comb[df_comb['Label'].isin(['Dynamic Rollover', 'LOW-G'])]
+#count each maneuver
+filtered_label_count = filtered_df['Label'].value_counts()
+
+#Create the bar plot
+plt.bar(filtered_label_count.index.astype(str), filtered_label_count.values, color='green')
+
+# Add Values above each bar in the graph
+for i, v in enumerate(filtered_label_count.values):
+    plt.text(i, v, str(v), ha='center', va='bottom')
+
+# Add labels and title to the chart
+#plt.xlabel('Maneuvers')
+plt.ylabel('Count')
+plt.title('Bar Chart of Maneuvers excluding None')
+
+# Display the chart
+plt.show()
+
+
+######################################
+# Create a bar plot of Data Types
+# Count the data types
+data_ty_count = df_comb.dtypes.value_counts()
+print(data_ty_count)
+
+# Create a bar plot of data type counts
+plt.bar(data_ty_count.index.astype(str), data_ty_count.values, color='green')
+
+# Add Values above each bar in the graph
+for i, v in enumerate(data_ty_count.values):
+    plt.text(i, v, str(v), ha='center', va='bottom')
+
+#plt.xlabel('Data Type')
+plt.ylabel('Count')
+plt.title('Number of Attributes by Data Type')
+
+# Display the plot
+plt.show()

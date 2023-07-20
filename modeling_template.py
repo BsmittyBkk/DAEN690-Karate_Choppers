18a472bdb3a3296d3cee0e96dbbdfa521c475886
mitema07 <91999841+mitema07@users.noreply.github.com>
2023-07-20 13:39:00 -0400

    Adding New dfs
    Adding new dfs to my repository.

--- modeling_template.py
+++ modeling_template.py
@@ -2,7 +2,7 @@ import pickle
 import pandas as pd
 
 # this is the path to your pickle file (should be the same location as CSVs)
-path = r'../CSV'
+path = r'C:\RotorCraftData\CSV'
 
 # the below function verifies that the dataframe you are working with is the same shape as the anticipated dataframe
 def test_dataframe_shape():
